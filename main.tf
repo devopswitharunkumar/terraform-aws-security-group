e7d12e386fa8455c639aed0b1d1c49ff7a9f4682
arunkumar261 <arunkumar261@gmail.com>
2026-07-16 19:10:50 +0530

    updated code

--- main.tf
+++ main.tf
@@ -6,22 +6,23 @@ resource "aws_security_group" "roboshop-sg" {
    dynamic "ingress" {
       for_each = var.sg_ingress_rules
       content {
-        description = ingress.value["description"]
-        from_port = ingress.value["from_port"]
-        to_port = ingress.value["to_port"]
-        protocol = ingress.value["protocol"]
-        cidr_blocks = ingress.value["cidr_blocks"]
+        description   = ingress.value["description"]
+        from_port     = ingress.value["from_port"]
+        to_port       = ingress.value["to_port"]
+        protocol      = ingress.value["protocol"]
+        cidr_blocks   = ingress.value["cidr_blocks"]
       }
     }
 
-#egress is same for every sg so keep static
+    # egress is always same for every sg, so keep egress static
     egress {
-    description     = "Allowing all outbount ports"
-    from_port       = 0
-    to_port         = 0
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        #ipv6_cidr_blocks = ["::/0"]
+    }
+
 
 
   tags = merge(
